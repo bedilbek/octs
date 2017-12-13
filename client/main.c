d471501f60a3833a0422199b08db09a2e767c6ee
Tom <isoqov12@gmail.com>
2017-12-13 16:21:47 +0500

    fix

--- client/main.c
+++ client/main.c
@@ -5,14 +5,14 @@
 
 int main() {
     setbuf(stdout, NULL);
-//    struct Client *client = new(Client, SERVER_LISTEN_PORT);
-    struct Client *file_client = new(Client, FILE_SERVER_LISTEN_PORT);
-//    cJSON *params = cJSON_CreateObject();
-//    cJSON_AddStringToObject(params, "name", "Tom");
-    cJSON *response = (cJSON *) send_file(file_client,
-                                          "/Users/tom1/Documents/Subject files/OS/Project/octs/client/client/client.c",
-                                          "client.c");
-//    cJSON *response = (cJSON *) send_message(client, 0, params);
+    struct Client *client = new(Client, SERVER_LISTEN_PORT);
+//    struct Client *file_client = new(Client, FILE_SERVER_LISTEN_PORT);
+    cJSON *params = cJSON_CreateObject();
+    cJSON_AddStringToObject(params, "name", "Tom");
+//    cJSON *response = (cJSON *) send_file(file_client,
+//                                          "/Users/tom1/Documents/Subject files/OS/Project/octs/client/client/client.c",
+//                                          "client.c");
+    cJSON *response = (cJSON *) send_message(client, 0, params);
 
     printf(cJSON_Print(response));
 }
