574120b7840783373adfcab65f6be2095c8f25c4
Ulugbek Abdullaev <ulugbekna@gmail.com>
2017-12-13 18:19:48 +0500

    signup changes

--- server/controllers/signup.c
+++ server/controllers/signup.c
@@ -23,10 +23,12 @@ cJSON *signup(cJSON *data) {
     char *email = cJSON_GetObjectItem(data, "email")->valuestring;
 
     if (valid_name(fname) && valid_name(lname) && valid_uname(username) && valid_pswd(password) && valid_email(email)) {
-        create_user_char(fname, lname, username, hash_pswd(password), email);
+        cJSON *query_result = create_user_char(fname, lname, username, hash_pswd(password), email);
+
     } else {
         setStatus(response, "400");
         setErrMsg(response, "Please, enter valid info.");
+        return response;
     }
 
     create_user_char(fname, lname, username, password, email);
