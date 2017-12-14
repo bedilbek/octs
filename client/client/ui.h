9e311036ec3d45317373a9e187c60c20058c6fad
shakhobiddin <urmanovshoxobiddin@gmail.com>
2017-12-14 12:34:51 +0500

    add ui

---
+++ client/client/ui.h
@@ -0,0 +1,20 @@
+//
+// Created by shakhobiddin on 12/13/17.
+//
+
+#ifndef OS_PROJECT_UI_H
+#define OS_PROJECT_UI_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include<cJSON.h>
+
+
+//cJSON *loginMenu();
+
+
+void saveToFileOcts(char *fname, char *lname, char *token, int id);
+
+#endif //OS_PROJECT_UI_H
+
--- client/main.c
+++ client/main.c
@@ -1,12 +1,56 @@
 ////
 //// Created by Tolqinbek Isoqov on 12/3/17.
 ////
-#include "client.h"
+//#include "client.h"
+#include "ui.c"
+
 
 int main() {
-    struct Client *client = new(Client, SERVER_LISTEN_PORT);
-    cJSON *params = cJSON_CreateObject();
-    cJSON_AddStringToObject(params, "name", "Tom");
-    cJSON *response = (cJSON *) send_message(client, 0, params);
-    printf(cJSON_Print(response));
+//    cJSON *params = cJSON_CreateObject();
+//    cJSON_AddStringToObject(params, "name", "Shoh");
+//    printf((char *)cJSON_parser(cJSON_GetObjectItem(params, "name")));
+
+
+    //struct Client *client = new(Client, SERVER_LISTEN_PORT);
+    //cJSON_AddNumberToObject(params, "id", 123);
+   // cJSON *response = (cJSON *) send_message(client, "get_user", params);
+  //  struct User u;
+//    saveToFileOcts("asd", "woxa", "234ewrw", 123);
+//
+//    makeUser();
+
+    isLoggedIn();
+//    printf("%s", u.token);
+
+
+    return 0;
 }
+
+/*
+ * $octs register
+ * octs submit main.c
+ * OK
+ * octs show-result
+octs //commands for user
+	register - register()
+	login - login()
+    contests - contests()
+    apply contest_id - applyForContest(contes_id)
+    problems - showProblems()
+    description problem_id -
+    standings contest_id - showStandings(contest_id)
+
+    submit file problem_id  - submit(argv[1], argv[2])
+
+
+
+	--problems
+	--see problem problem_id
+octs //commands for admin
+	--add new problem
+	--submissions
+	--problems
+	--create contest
+*/
+
+//login - login
